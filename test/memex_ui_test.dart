14bcfadc351c1ff355715af94911ebd17b599cd6
Pit Hüne <pitsimonhuene@t-online.de>
2023-02-10 17:52:45 +0100

    feat: Add tests

--- test/memex_ui_test.dart
+++ test/memex_ui_test.dart
@@ -1,12 +1,67 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/painting.dart';
+import 'package:test/test.dart';
 
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+import 'package:memex_ui/editor/block.dart';
+import 'package:memex_ui/editor/block_path.dart';
+import 'package:memex_ui/editor/cursor.dart';
 import 'package:memex_ui/memex_ui.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('delete across piece boundary', () {
+    var state = EditorState(
+      blocks: <EditorBlock>[
+        ParagraphBlock(<TextSpan>[
+          const TextSpan(text: "foo"),
+          const TextSpan(text: "bar"),
+          EditorBlock.sentinelPiece,
+        ].lockUnsafe),
+      ].lockUnsafe,
+      selection: Selection(
+        end: Cursor(
+          blockPath: BlockPath([0].lockUnsafe),
+          pieceIndex: 0,
+          offset: 0,
+        ),
+      ),
+    );
+    expect(state.selection.isEmpty, true);
+    state = state.moveCursorRightOnce(false);
+    state = state.moveCursorRight(4, true);
+    expect(
+      state.selection.start,
+      Cursor(
+        blockPath: BlockPath([0].lockUnsafe),
+        pieceIndex: 0,
+        offset: 1,
+      ),
+    );
+    expect(
+      state.selection.end,
+      Cursor(
+        blockPath: BlockPath([0].lockUnsafe),
+        pieceIndex: 1,
+        offset: 2,
+      ),
+    );
+    state = state.deleteBackwards();
+    expect(
+      state.blocks[0].pieces,
+      <TextSpan>[
+        const TextSpan(text: "f"),
+        const TextSpan(text: "r"),
+        EditorBlock.sentinelPiece,
+      ].lockUnsafe,
+    );
+    expect(state.selection.isEmpty, true);
+    expect(state.blocks[0].pieces[1].text!.length, 1);
+    expect(
+      state.cursor,
+      Cursor(
+        blockPath: BlockPath([0].lockUnsafe),
+        pieceIndex: 1,
+        offset: 0,
+      ),
+    );
   });
 }
