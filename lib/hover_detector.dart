5ebc4de54780cd9e7037d565ea1b04e560ee9a07
Pit Hüne <pitsimonhuene@t-online.de>
2023-03-13 13:37:36 +0100

    feat: Add cursor parameter to HoverDetector

--- lib/hover_detector.dart
+++ lib/hover_detector.dart
@@ -3,10 +3,12 @@ import 'package:flutter/widgets.dart';
 class HoverDetector extends StatefulWidget {
   final Widget? child;
   final Widget Function(BuildContext, bool, Widget?) builder;
+  final MouseCursor? cursor;
   const HoverDetector({
     super.key,
     required this.builder,
     this.child,
+    this.cursor,
   });
 
   @override
@@ -19,6 +21,7 @@ class _HoverDetectorState extends State<HoverDetector> {
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
+      cursor: widget.cursor ?? MouseCursor.defer,
       onEnter: (event) {
         if (isHovered) return;
         setState(() {
