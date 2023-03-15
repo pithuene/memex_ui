2c0df1a56d6efdb84e7cc902407f321205e55434
Pit Hüne <pitsimonhuene@t-online.de>
2023-03-15 16:29:27 +0100

    feat: Add header widget to editor view

--- lib/editor/editor_view.dart
+++ lib/editor/editor_view.dart
@@ -17,11 +17,16 @@ class EditorView extends StatefulWidget {
     this.scrollController,
     this.keymap = const KeymapStandard(),
     this.linkHandler,
+    this.header,
   });
   final Editor editor;
   final ScrollController? scrollController;
   final Keymap keymap;
   final LinkHandler? linkHandler;
+
+  /// A widget which is shown above the editor content but inside the scrollable area.
+  /// Can be used to display a document title.
+  final Widget? header;
 
   @override
   State<StatefulWidget> createState() => _EditorViewState();
@@ -75,29 +80,38 @@ class _EditorViewState extends State<EditorView> {
           //if (needRebuild) setState(() {});
           setState(() {});
         },
-        child: ListView.builder(
-          padding: const EdgeInsets.all(20),
-          controller: widget.scrollController,
-          itemCount: widget.editor.state.blocks.length,
-          itemBuilder: (context, index) {
-            return Center(
-              child: Container(
-                constraints: const BoxConstraints(maxWidth: 750),
-                alignment: Alignment.topLeft,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: PaddedBlock(
-                        path: BlockPath.fromIterable([index]),
-                        block: widget.editor.state.blocks[index],
-                        editor: widget.editor,
+        child: ScrollConfiguration(
+          behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+          child: ListView.builder(
+            controller: widget.scrollController,
+            itemCount: widget.editor.state.blocks.length +
+                (widget.header != null ? 1 : 0),
+            itemBuilder: (context, index) {
+              Widget child;
+              if (widget.header != null && index == 0) {
+                child = widget.header!;
+              } else {
+                child = PaddedBlock(
+                  path: BlockPath.fromIterable([index - 1]),
+                  block: widget.editor.state.blocks[index - 1],
+                  editor: widget.editor,
+                );
+              }
+              return Center(
+                child: Container(
+                  constraints: const BoxConstraints(maxWidth: 750),
+                  alignment: Alignment.topLeft,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: child,
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
     );
