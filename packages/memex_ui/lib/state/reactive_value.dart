e43d9661fc67e50d5deaeda88fddc3e13d55f9b4
Pit Hüne <pitsimonhuene@t-online.de>
2024-01-23 22:08:24 +0100

    feat(ui): Finally make Const ReactiveValue const
    This enables Const to be used as a default constructor parameter for
    ReactiveValue properties.

--- packages/memex_ui/lib/state/reactive_value.dart
+++ packages/memex_ui/lib/state/reactive_value.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:memex_ui/state/state.dart';
 
-abstract class ReactiveValue<T> extends ChangeNotifier
-    implements ValueListenable<T> {}
+abstract class ReactiveValue<T> implements Listenable, ValueListenable<T> {}
 
-class Prop<T> extends ReactiveValue<T> {
+class Prop<T> extends ChangeNotifier implements ReactiveValue<T> {
   T _value;
   Prop(this._value);
 
@@ -23,11 +22,17 @@ class Prop<T> extends ReactiveValue<T> {
 /// A [ReactiveValue] which isn't actually mutable.
 /// Useful when setting properties which expect a [ReactiveValue]
 /// to a constant value.
-class Const<T> extends ReactiveValue<T> {
+class Const<T> implements ReactiveValue<T> {
   final T _value;
 
-  Const(this._value);
+  const Const(this._value);
 
   @override
   T get value => _value;
+
+  @override
+  void addListener(VoidCallback listener) {}
+
+  @override
+  void removeListener(VoidCallback listener) {}
 }
